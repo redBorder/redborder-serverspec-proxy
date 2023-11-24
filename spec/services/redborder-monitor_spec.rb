0b3cc24feae37a6077f0bcc6603d7f1b09e3b6b8
Miguel Negron <manegron@redborder.com>
2023-11-24 14:47:56 +0000

    Initial version

--- spec/services/redborder-monitor_spec.rb
+++ spec/services/redborder-monitor_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 set :os, :family => 'redhat', :release => '9', :arch => 'x86_64'
 
-describe package('nginx') do
+describe package('redborder-monitor') do
   it { should be_installed }
 end
 
-describe service('nginx') do
+describe service('redborder-monitor') do
   it { should be_enabled }
   it { should be_running }
 end
