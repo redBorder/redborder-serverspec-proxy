cfe63323c8fc71b49e0af786aa47c00e31cc90ba
Miguel Negron <manegron@redborder.com>
2023-12-12 10:30:31 +0000

    add journald tests

---
+++ spec/services/systemd_journald_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+set :os, family: 'redhat', release: '9', arch: 'x86_64'
+
+
+describe 'Checking systemd-journald' do
+  describe service('systemd-journald') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe 'Configuration' do
+    describe file('/etc/systemd/journald.conf') do
+      it { should exist }
+      it { should contain 'Storage=volatile' }
+    end
+  end
+end
