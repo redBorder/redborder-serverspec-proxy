ec2372cf3904a1111e28335010779a607e1cdbc4
Luis Blanco <ljblanco@redborder.com>
2024-06-13 16:44:56 +0100

    description change

--- spec/users/users_spec.rb
+++ spec/users/users_spec.rb
@@ -14,7 +14,7 @@ describe 'Checking only these users has login permission' do
   allowed_users = Set.new %w[root redborder]
   not_allowed_users = bash_users - allowed_users
 
-  it 'should only allow specified users to have login permissions' do
+  it 'This set is supposed to be empty' do
     expect(not_allowed_users).to be_empty
   end
 end
