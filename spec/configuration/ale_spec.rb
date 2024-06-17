7b1bd5d6fff7eb5305101cb04de525fc10b515d2
Luis Blanco <ljblanco@redborder.com>
2024-06-17 09:07:42 +0100

    copy from manager: check config only if package is installed

--- spec/configuration/ale_spec.rb
+++ spec/configuration/ale_spec.rb
@@ -3,9 +3,37 @@
 require 'spec_helper'
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-# TODO: only do it if ale  is enabled
-describe 'Check ale config' do
-  describe file('/etc/redborder-ale/schema.pb.rb') do
+is_package_installed = package('redborder-ale').installed?
+
+if is_package_installed
+  describe 'Check ale config' do
+    describe file('/etc/redborder-ale/schema.pb.rb') do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe file('/etc/redborder-ale/config.yml') do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe file('/etc/redborder-ale/rb_ale_aps.conf') do
+      it { should exist }
+      it { should be_file }
+    end
+
+    # Additional dependencies
+    describe file('/usr/lib64/libzmq.so') do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe package('zeromq-devel') do
+      it { should be_installed }
+    end
+  end
+
+  describe file('/var/rb-ale/bin/rb_ale.rb') do
     it { should exist }
     it { should be_file }
   end
