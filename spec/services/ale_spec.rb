8674628aef2a29c57539e0573f59d0457a754819
Luis Blanco <ljblanco@redborder.com>
2024-08-01 17:15:37 +0100

    comment port until we know if its really necessary

--- spec/services/ale_spec.rb
+++ spec/services/ale_spec.rb
@@ -2,24 +2,44 @@
 
 require 'spec_helper'
 require 'set'
+
 set :os, family: 'redhat', release: '9', arch: 'x86_64'
 
-service = 'redborder-ale'
-is_package_installed = package('redborder-ale').installed?
+service = pkg = 'redborder-ale'
+# port = 7779
+API_ENDPOINT = 'http://localhost:8500/v1'
 
-if is_package_installed
-  service_status = command("systemctl is-enabled #{service}").stdout.strip
-  describe "Checking #{service_status} service for #{service}..." do
-    if service_status == 'enabled'
-      describe service(service) do
-        it { should be_enabled }
-        it { should be_running }
-      end
-    elsif service_status == 'disabled'
-      describe service(service) do
-        it { should_not be_enabled }
-        it { should_not be_running }
-      end
+describe "Checking packages for #{service}..." do
+  describe package(pkg) do
+    before do
+      skip("#{pkg} is not installed, skipping...") unless package(pkg).installed?
     end
+
+    it 'is expected to be installed' do
+      expect(package(pkg).installed?).to be true
+    end
+  end
+end
+
+service_status = command("systemctl is-enabled #{service}").stdout.strip
+describe "Checking #{service_status} service for #{service}..." do
+  if service_status == 'enabled'
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    # describe port(port) do
+    #   it { should be_listening }
+    # end
+  elsif service_status == 'disabled'
+    describe service(service) do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+
+    # describe port(port) do
+    #   it { should_not be_listening }
+    # end
   end
 end
