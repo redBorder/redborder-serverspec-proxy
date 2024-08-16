ecd705d70806c87ee1e0e936cd598135667a2070
Luis Blanco <ljblanco@redborder.com>
2024-08-16 15:55:12 +0000

    lint

--- spec/services/logstash_spec.rb
+++ spec/services/logstash_spec.rb
@@ -15,37 +15,24 @@ PIPELINES_PATH = '/etc/logstash/pipelines.yml'
 
 describe "Checking service status for #{service}..." do
   regex = '^- pipeline\.id: .*-pipeline$'
-  has_pipelines = command("grep --perl-regex '#{regex}' #{PIPELINES_PATH}")
+  has_pipelines = command("grep --perl-regex '#{regex}' #{PIPELINES_PATH}").exit_status == 0
+  expected_state = has_pipelines      ? :be_enabled   : :not_be_enabled
+  expected_running = has_pipelines    ? :be_running   : :not_be_running
+  expected_listening = has_pipelines  ? :be_listening : :not_be_listening
+  expected_installed = has_pipelines  ? :be_installed : :not_be_installed
 
-  unless has_pipelines.exit_status == 0
-    describe service(service) do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
-    describe port(port) do
-      it { should_not be_listening }
-    end
-
-    packages.each do |package|
-      describe package(package) do
-        it { should_not be_installed }
-      end
-    end
+  describe service(service) do
+    it { should send(expected_state) }
+    it { should send(expected_running) }
   end
 
-  if has_pipelines.exit_status == 0
-    describe service(service) do
-      it { should be_enabled }
-      it { should be_running }
-    end
-    describe port(port) do
-      it { should be_listening }
-    end
+  describe port(port) do
+    it { should send(expected_listening) }
+  end
 
-    packages.each do |package|
-      describe package(package) do
-        it { should be_installed }
-      end
+  packages.each do |package|
+    describe package(package) do
+      it { should send(expected_installed) }
     end
   end
 end
