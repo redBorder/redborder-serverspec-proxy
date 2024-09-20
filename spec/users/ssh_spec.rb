bb0e5e25aec2fc927b0d8b8d9ee61d1062516f35
Miguel Negron <manegron@redborder.com>
2024-09-20 16:14:56 +0100

    Remove ssh user test

--- spec/users/ssh_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-set :os, family: 'redhat', release: '9', arch: 'x86_64'
-
-describe user('sshd') do
-  it { should exist }
-  it { should have_login_shell('/sbin/nologin') }
-end
